8cff4e36a6116d9b1f3b29b02c04b6f0af1cd496
cf <cf@ftpd.se>
2017-04-19 08:13:19 +0200

    Updated functions

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
 download_url=https://dl.ubnt.com/firmwares/unifi-video/3.6.3/unifi-video_3.6.3~Debian7_amd64.deb
+file_name=unifi-video_3.6.3~Debian7_amd64.deb
+ip=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
-#Update and upgrade
-sudo /usr/bin/apt-get -V update -f -y
-sudo /usr/bin/apt-get -V dist-upgrade -f -y
-
-#download unifi video package
+sudo /usr/bin/apt-get -V update -f -y --fix-missing
+sudo /usr/bin/apt-get -V upgrade -f -y
 wget $download_url
+sudo /usr/bin/apt-get -f -y install
+sudo dpkg -i $file_name
+rm $file_name
 
-sudo /usr/bin/apt-get -f install
-
-dpkg -i unifi-video_3.6.3~Debian7_amd64.deb
+echo "Install complete - installation file deleted."
+echo "The new interface can be accessed at https://"$ip":7443" 
