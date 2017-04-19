018f7aedcad9a20e5997ab8adc3b8f8eaa06be4f
cf <cf@ftpd.se>
2017-04-19 08:21:00 +0200

    Updated functions

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ download_url=https://dl.ubnt.com/firmwares/unifi-video/3.6.3/unifi-video_3.6.3~D
 file_name=unifi-video_3.6.3~Debian7_amd64.deb
 ip=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
-sudo /usr/bin/apt-get -V update -f -y --fix-missing
-sudo /usr/bin/apt-get -V upgrade -f -y
+sudo /usr/bin/apt-get update -y --fix-missing
+sudo /usr/bin/apt-get upgrade -f -y
 wget $download_url
 sudo /usr/bin/apt-get -f -y install
 sudo dpkg -i $file_name
